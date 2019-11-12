01167d41b6f9ce3ca4d6dc21cf2d488fc31fd1b4
John Wohlbier <jgwohlbier@sei.cmu.edu>
2019-11-12 15:59:20 -0500

    Revert "put local_arena_allocator.h into the source."
    This reverts commit 5d6f5864c1aaac9d68383954e66c70521b499165.

--- main.cc
+++ main.cc
@@ -18,119 +18,7 @@ extern "C" {
 #define THREADS_PER_NODELET \
     THREAD_OVERSUBSCRIBE * THREADS_PER_GC * GC_PER_NODELET
 
-#include <emu_c_utils/emu_c_utils.h>
-#include <cassert>
-#include <cstddef>
-#include <memory>
-
-namespace emu {
-
-    class local_arena
-    {
-    protected:
-	typedef unsigned char uchar;
-	// Value originally returned from mw_mallocrepl
-	void * buffer;
-	// Pointer to next available byte on this nodelet
-	uchar * next_chunk;
-	// Size of each stripe, in elements
-	size_t size;
-    public:
-
-	// @param stripe_size: Number of bytes to reserve on each nodelet
-	local_arena(size_t stripe_size)
-	{
-            assert(stripe_size > 0);
-            // Allocate a stripe on each nodelet
-            void * ptr = mw_mallocrepl(stripe_size);
-            assert(ptr); // FIXME
-            // Save the pointer in all copies of the allocator
-            // since we don't know which one will get freed
-            mw_replicated_init((long*)&buffer, (long)ptr);
-            mw_replicated_init((long*)&size, (long)stripe_size);
-
-            // Let the nth copy of next_chunk point to the stripe on the nth nodelet
-            for (long nlet = 0; nlet < NODELETS(); ++nlet) {
-                uchar * nth_chunk = (uchar*)mw_get_nth(buffer, nlet);
-                uchar ** nth_ptr = (uchar**)mw_get_nth(&next_chunk, nlet);
-                *nth_ptr = nth_chunk;
-            }
-	}
-
-	~local_arena()
-	{
-            mw_free(buffer);
-	}
-
-	void *
-            allocate(size_t n, const void * hint)
-	{
-            if (hint) { MIGRATE((void*)hint); }
-            // Serial version
-            void * ptr = next_chunk;
-            next_chunk += n;
-            // Parallel-safe version
-            // T * ptr = (T*)ATOMIC_ADDMS((long*)next_chunk, (long)(n * sizeof(T)));
-            return ptr;
-	}
-    };
-
-// Reserve 2GB on each nodelet for satisfying allocations
-    extern replicated local_arena g_arena;
-
-    template<typename T>
-        class local_arena_allocator
-    {
-    protected:
-	local_arena & arena;
-    public:
-	typedef T value_type;
-
-        // Declare friendship with all templated versions of this class
-        template<typename U>
-            friend class local_arena_allocator;
-
-        // Default constructor, uses the global replicated arena
-    local_arena_allocator() : arena(g_arena) {}
-
-        // Alternate constructor for specifying a different arena to use
-	explicit
-            local_arena_allocator(local_arena & arena) : arena(arena) {}
-
-        // Templated copy-constructor, for use with rebind
-        template<typename U>
-            local_arena_allocator(const local_arena_allocator<U>& other) : arena(other.arena) {}
-
-        template<typename U>
-            bool operator== (const local_arena_allocator<U>& other)
-        {
-	    return arena == other.arena;
-        }
-
-	T *
-            allocate(size_t n)
-	{
-            return static_cast<T*>(
-                arena.allocate(n * sizeof(T), nullptr)
-		);
-	}
-
-	T *
-            allocate(size_t n, const void * hint)
-	{
-            return static_cast<T*>(
-                arena.allocate(n * sizeof(T), hint)
-		);
-	}
-
-	void
-            deallocate(T *, size_t)
-	{
-            // Not implemented
-	}
-};
-
-} // end namespace emu
+#include "local_arena_allocator.h"
 
 #include <tuple>
 #include <vector>
@@ -151,13 +39,10 @@ typedef std::vector<Pair_t, emu::local_arena_allocator<Pair_t>> Row_t;
 typedef Row_t * pRow_t;
 typedef pRow_t * ppRow_t;
 
-//static inline
-Index_t n_map(Index_t i) { return i % NODELETS(); }
-//static inline
-Index_t r_map(Index_t i) { return i / NODELETS(); }
+static inline Index_t n_map(Index_t i) { return i % NODELETS(); }
+static inline Index_t r_map(Index_t i) { return i / NODELETS(); }
 // inverse mapping
-//static inline
-Index_t nr_inv(Index_t n, Index_t r)
+static inline Index_t nr_inv(Index_t n, Index_t r)
 { return r * NODELETS() + n; }
 
 /*
@@ -312,7 +197,7 @@ private:
 };
 typedef rMatrix_t * prMatrix_t;
 
-//static inline
+static inline
 bool index_exists(pRow_t r, Index_t icol)
 {
     bool result = false;
@@ -329,7 +214,7 @@ bool index_exists(pRow_t r, Index_t icol)
     return result;
 }
 
-//static inline
+static inline
 bool dot(Scalar_t & ans, pRow_t a, pRow_t b) // no semiring
 {
     bool result = false;
@@ -361,7 +246,7 @@ bool dot(Scalar_t & ans, pRow_t a, pRow_t b) // no semiring
     return result;
 }
 
-//static inline
+static inline
 void row_kernel(Index_t irow,
                 prMatrix_t C,
                 prMatrix_t const M,
@@ -391,7 +276,7 @@ void row_kernel(Index_t irow,
     }
 }
 
-//static inline
+static inline
 void multi_row_kernel(Index_t nl_id,
                       Index_t t,
                       Index_t nrow,
@@ -409,7 +294,7 @@ void multi_row_kernel(Index_t nl_id,
 
 }
 
-//static inline
+static inline
 void ABT_Mask_NoAccum_kernel(
     Index_t nl_id,              // nodelet_id
     prMatrix_t C,               // output matrix
